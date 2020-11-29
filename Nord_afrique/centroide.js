30884e1c4f45a7315957327928c39b6875dc67e4
YOUFERCHACH685 <74077189+YOUFERCHACH685@users.noreply.github.com>
2020-11-29 14:19:34 +0100

    Add files via upload

---
+++ Nord_afrique/centroide.js
@@ -0,0 +1,12 @@
+var centroide = {
+"type": "FeatureCollection",
+"name": "centroides",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 5, "COUNTRY": "Egypte", "ISO_CC": "EG", "Population": 56133439, "Area": 1110660.0, "PERTE_FORET_2001_2006": 94.97, "PERTE_FORET_2007_2013": 136.57, "PERTE_FORET_2014_2019": 54.24 }, "geometry": { "type": "Point", "coordinates": [ 29.8723, 26.4941 ] } },
+{ "type": "Feature", "properties": { "Id": 2, "COUNTRY": "Algeria", "ISO_CC": "DZ", "Population": 34222570, "Area": 2673130.0, "PERTE_FORET_2001_2006": 4094.86, "PERTE_FORET_2007_2013": 7490.02, "PERTE_FORET_2014_2019": 13545.91 }, "geometry": { "type": "Point", "coordinates": [ 2.665, 28.1611 ] } },
+{ "type": "Feature", "properties": { "Id": 4, "COUNTRY": "Libye", "ISO_CC": "LY", "Population": 6169824, "Area": 1819250.0, "PERTE_FORET_2001_2006": 21.84, "PERTE_FORET_2007_2013": 17.24, "PERTE_FORET_2014_2019": 3.44 }, "geometry": { "type": "Point", "coordinates": [ 18.027, 27.044 ] } },
+{ "type": "Feature", "properties": { "Id": 1, "COUNTRY": "Maroc", "ISO_CC": "MA", "Population": 33848242, "Area": 802028.0, "PERTE_FORET_2001_2006": 23170.51, "PERTE_FORET_2007_2013": 2003.74, "PERTE_FORET_2014_2019": 1728.36 }, "geometry": { "type": "Point", "coordinates": [ -8.8992, 29.1181 ] } },
+{ "type": "Feature", "properties": { "Id": 3, "COUNTRY": "Tunisie", "ISO_CC": "TN", "Population": 8620181, "Area": 203144.0, "PERTE_FORET_2001_2006": 553.15, "PERTE_FORET_2007_2013": 1127.93, "PERTE_FORET_2014_2019": 2620.22 }, "geometry": { "type": "Point", "coordinates": [ 9.561564, 34.11124 ] } }
+]
+}
